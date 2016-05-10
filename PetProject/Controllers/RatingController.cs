e63984e7f9faa76b0106c9869441fd36c4f45edc
Nick S <ns3205@nyu.edu>
2016-05-09 20:50:44 -0700

    commit

---
+++ PetProject/Controllers/RatingController.cs
@@ -0,0 +1,22 @@
+﻿using PetProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetProject.Controllers
+{
+    public class RatingController : Controller
+    {
+        // GET: Rating
+        public ActionResult Index()
+        {
+            using (var res = new QueryContext())
+            {
+                IList<result> mod = res.GetTopSongs1(DateTime.Now, DateTime.Now.AddMonths(7), 10);
+                return View(mod);
+            }
+        }
+    }
+}
